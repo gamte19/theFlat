2145a8b11cfa4a0f26ccc9b91cbb61962927fff7
gamte19 <gamte19@gmail.com>
2017-07-04 14:23:41 +0200

    Spring project

---
+++ src/main/java/com/flat/FlatApplication.java
@@ -0,0 +1,13 @@
+package com.flat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatApplication.class, args);
+	}
+}
